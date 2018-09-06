f9d6c8bdc0fa8ea7ac5672f7e1e8e106e96aac03
Kristy Mae Almuete <kristyalmuete@gmail.com>
2018-09-06 10:24:50 +0800

    #1 Update Angular version from 5 to 6 and generate new project directory

--- public/uploading-app/e2e/src/app.e2e-spec.ts
+++ public/uploading-app/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('app App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('app App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect<any>(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to uploading-app!');
   });
 });
