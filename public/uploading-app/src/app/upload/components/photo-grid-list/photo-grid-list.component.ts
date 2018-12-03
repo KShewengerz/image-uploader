00eab388772ebf508f5df9e104a7f47671aa4ebd
Kristy Mae Almuete <kristyalmuete@gmail.com>
2018-12-03 20:05:04 +0800

    #14 Setup Photo Grid UI using @material/image-list - Masonry layout.

--- public/uploading-app/src/app/upload/components/photo-grid-list/photo-grid-list.component.ts
+++ public/uploading-app/src/app/upload/components/photo-grid-list/photo-grid-list.component.ts
@@ -7,7 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./photo-grid-list.component.scss']
 })
 export class PhotoGridListComponent {
-
+  
+  baseImageUrl: string = 'https://s3.amazonaws.com/sample-v1';
+  
+  imageLinks: string[] = [
+    `${this.baseImageUrl}/architecture.jpg`,
+    `${this.baseImageUrl}/boat.jpg`,
+    `${this.baseImageUrl}/bonding.jpg`,
+    `${this.baseImageUrl}/books.jpg`,
+    `${this.baseImageUrl}/family.jpg`,
+    `${this.baseImageUrl}/overlooking.jpg`,
+    `${this.baseImageUrl}/overview.jpg`,
+    `${this.baseImageUrl}/river.jpg`,
+    `${this.baseImageUrl}/rocks.jpg`,
+    `${this.baseImageUrl}/wedding.jpg`
+  ];
+  
   constructor() { }
 
 }
