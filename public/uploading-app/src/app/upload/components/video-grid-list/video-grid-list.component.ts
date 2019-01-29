e7ca3ae617baafa6d1511683c447e3a030589d9d
Kristy Mae Almuete <kristyalmuete@gmail.com>
2019-01-29 20:02:05 +0800

    #15 Fix Video List Responsiveness and Add Breakpoint Observer Functionality

--- public/uploading-app/src/app/upload/components/video-grid-list/video-grid-list.component.ts
+++ public/uploading-app/src/app/upload/components/video-grid-list/video-grid-list.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+import { Observable, combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/layout';
 
 import { Video } from '@app/upload/models/interfaces/video.interface';
 
@@ -9,14 +14,65 @@ import { Video } from '@app/upload/models/interfaces/video.interface';
   templateUrl : './video-grid-list.component.html',
   styleUrls   : ['./video-grid-list.component.scss']
 })
-export class VideoGridListComponent {
+export class VideoGridListComponent implements OnInit {
   
-  gridListOptions = { cols: 3, rowHeight: '280px', gutterSize: '20px' };
-  gridRowColSpan: number  = 1;
-  videoWidth: string = '400';
+  isWebLandscape       : Observable<BreakpointState>;
+  isWebPortrait        : Observable<BreakpointState>;
+  isTabletLandscape    : Observable<BreakpointState>;
+  isTabletPortrait     : Observable<BreakpointState>;
+  isHandsetLandscape   : Observable<BreakpointState>;
+  isHandsetPortrait    : Observable<BreakpointState>;
+  isMaxWidth599Screen  : Observable<BreakpointState>;
+  isMaxWidth425Screen  : Observable<BreakpointState>;
+  
+  rowHeight : string;
+  gridColumn: number;
   
   videos: Video[] = this.route.snapshot.data.videos;
 
-  constructor(private route: ActivatedRoute) {}
-
+  constructor(private route: ActivatedRoute,
+              private breakpointObserver: BreakpointObserver) {}
+              
+  ngOnInit(): void {
+    this.initializeBreakpointObservers();
+  }
+  
+  initializeBreakpointObservers(): void {
+    const observe = breakpoint => this.breakpointObserver.observe(breakpoint);
+    
+    this.isWebLandscape      = observe(Breakpoints.WebLandscape);
+    this.isWebPortrait       = observe(Breakpoints.WebPortrait);
+    this.isTabletLandscape   = observe(Breakpoints.TabletLandscape);
+    this.isTabletPortrait    = observe(Breakpoints.TabletPortrait);
+    this.isHandsetLandscape  = observe(Breakpoints.HandsetLandscape);
+    this.isHandsetPortrait   = observe(Breakpoints.HandsetPortrait);
+    this.isMaxWidth599Screen = observe('(max-width: 599px)');
+    this.isMaxWidth425Screen = observe('(max-width: 425px)');
+    
+    this.observeBreakpoints();
+  }
+  
+  observeBreakpoints(): void {
+    const matches            = breakpoints => breakpoints.map(({ matches }) => matches);
+    
+    const combineBreakpoints = [
+      this.isWebLandscape,
+      this.isWebPortrait,
+      this.isTabletLandscape,
+      this.isTabletPortrait,
+      this.isHandsetLandscape,
+      this.isHandsetPortrait,
+      this.isMaxWidth599Screen,
+      this.isMaxWidth425Screen
+    ];
+    
+    combineLatest(...combineBreakpoints)
+      .pipe(map(breakpoints => matches(breakpoints)))
+      .subscribe(([ webL, webP, tabletL, tabletP, handsetL, handsetP, s599, s425 ]) => {
+        this.gridColumn = webL || tabletL ? 4 : webP || handsetL ? 3 : tabletP ? 2 : 1;
+        this.rowHeight  = s425 ? '1:0.7' : s599 ? '1:0.6' : '1:0.8';
+      });
+    
+  }
+  
 }
